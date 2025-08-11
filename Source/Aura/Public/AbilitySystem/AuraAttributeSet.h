b7721907b3037c6b743d1eb1a223405a81dcc8aa
Alejandro <alejandro.suau@gmail.com>
2025-08-11 15:36:54 +0200

    Add mana and potion curve values per level

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -5,7 +5,6 @@
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
 #include "AbilitySystemComponent.h"
-#include "AbilitySystemTestAttributeSet.h"
 #include "AuraAttributeSet.generated.h"
 
 #define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
@@ -13,6 +12,41 @@
 	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
 	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
 	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+struct FGameplayEffectModCallbackData;
+
+USTRUCT()
+struct FEffectProperties
+{
+	GENERATED_BODY()
+
+	FEffectProperties() = default;
+	FGameplayEffectContextHandle EffectContextHandle;
+
+	UPROPERTY()
+	UAbilitySystemComponent* SourceASC {nullptr};
+
+	UPROPERTY()
+	AActor* SourceAvatarActor {nullptr};
+
+	UPROPERTY()
+	AController* SourceController {nullptr};
+
+	UPROPERTY()
+	ACharacter* SourceCharacter {nullptr};
+	
+	UPROPERTY()
+	UAbilitySystemComponent* TargetASC {nullptr};
+
+	UPROPERTY()
+	AActor* TargetAvatarActor {nullptr};
+
+	UPROPERTY()
+	AController* TargetController {nullptr};
+
+	UPROPERTY()
+	ACharacter* TargetCharacter {nullptr};
+};
 
 /**
  * 
@@ -25,6 +59,9 @@ class AURA_API UAuraAttributeSet : public UAttributeSet
 public:
 	UAuraAttributeSet();
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
 	FGameplayAttributeData Health;
@@ -53,4 +90,7 @@ public:
 
 	UFUNCTION()
 	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+
+private:
+	void SetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const;
 };
